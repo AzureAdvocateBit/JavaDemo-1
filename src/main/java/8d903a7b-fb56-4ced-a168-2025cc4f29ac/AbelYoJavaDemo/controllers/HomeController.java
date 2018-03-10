86517792c4abea3ff0ac6fea3263becc99b9836b
Abel <abel.wang@gmail.com>
2018-03-10 13:52:12 -0800

    initial checkin

---
+++ src/main/java/8d903a7b-fb56-4ced-a168-2025cc4f29ac/AbelYoJavaDemo/controllers/HomeController.java
@@ -0,0 +1,32 @@
+package 8d903a7b-fb56-4ced-a168-2025cc4f29ac.AbelYoJavaDemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/")
+	public ModelAndView index() {
+		ModelAndView result = new ModelAndView("index");
+		result.addObject("Title", "Home Page");
+		return result;
+	}
+	
+	@RequestMapping(value="/about")
+	public ModelAndView about() {
+		ModelAndView result = new ModelAndView("about");
+		result.addObject("Title", "About");
+		result.addObject("Message", "Your application description page.");
+		return result;
+	}
+	
+	@RequestMapping(value="/contact")
+	public ModelAndView contact() {
+		ModelAndView result = new ModelAndView("contact");
+		result.addObject("Title", "Contact");
+		result.addObject("Message", "Your contact page.");
+		return result;
+	}
+}
